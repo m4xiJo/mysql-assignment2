c208e8fe6cb70ad490e47d8d5efaf3638d1d4574
Max J <14187946+m4xiJo@users.noreply.github.com>
2021-05-11 14:37:41 +0200

    updates

--- procedure.sql
+++ procedure.sql
@@ -2,11 +2,18 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS AddToCart;
 CREATE PROCEDURE AddToCart(IN customer_id_param INT, order_id_param INT, product_id_param INT)
 BEGIN
+	
 	DECLARE check_order_id int;
     DECLARE check_product_id int;
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION, SQLWARNING
+	BEGIN
+		ROLLBACK;
+		RESIGNAL;
+	END;
+    -- SET autocommit = 0;
+    START TRANSACTION;
 	SELECT orders.order_id INTO check_order_id FROM orders WHERE order_id = order_id_param;
 	SELECT products.product_id INTO check_product_id FROM products WHERE product_id = product_id_param;
-    
     IF (check_order_id IS NULL OR order_id_param IS NULL) THEN
 		INSERT INTO Orders (customer_id) VALUES (customer_id_param);
         INSERT INTO ProductsPerOrders (product_id, quantity, order_id) VALUES (product_id_param, 1, LAST_INSERT_ID());
@@ -15,15 +22,18 @@ BEGIN
 		INSERT INTO ProductsPerOrders (product_id, quantity, order_id) VALUES (product_id_param, 1, order_id_param);
         UPDATE Products SET instock = instock - 1 WHERE product_id = product_id_param;
     ELSEIF (check_order_id = order_id_param AND check_product_id = product_id_param) THEN
-		UPDATE ProductsPerOrders SET quantity = quantity + 1 WHERE order_id = order_id_pramam AND product_id = product_id_param;
+		UPDATE ProductsPerOrders SET quantity = quantity + 1 WHERE order_id = order_id_param AND product_id = product_id_param;
         UPDATE Products SET instock = instock - 1 WHERE product_id = product_id_param;
 	END IF;
+    COMMIT;
 END //
 
-DECLARE EXIT HANDLER FOR 1062 
-BEGIN
-	ROLLBACK;
-	RESIGNAL SET MESSAGE_TEXT = "Unique onstraint broken";
-END;
-
--- CALL AddToCart (1, null, 1);
+DROP TRIGGER IF EXISTS before_product_update;
+CREATE TRIGGER before_product_update 
+	AFTER UPDATE ON Products 
+	FOR EACH ROW
+    BEGIN
+		IF NEW.instock = 0 THEN
+			INSERT INTO OutOfStock (product_id) VALUES (NEW.product_id);
+		END IF;
+    END;
--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,73 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.*;
 public class Main {
+    protected String url;
+    protected String user;
+    protected String passwd;
+    protected Connection conn;
+
     public Main() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://127.0.0.1:3306/default_schema";
-        String user = "admin";
-        String passwd = "minininja123";
-        Connection conn = DriverManager.getConnection(url, user, passwd);
+        url = "jdbc:mysql://127.0.0.1:3306/default_schema";
+        user = "admin";
+        passwd = "minininja123";
+        conn = DriverManager.getConnection(url, user, passwd);
     }
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
-        String userName = getInput("Type your username: ");
+
+        String userName = getInput("Type your firstname: ");
         String userPasswd = getInput("Type your password: ");
 
-        String make = getInput("Type the make of the product to add to cart: ");
-        String model = getInput("Type the model of the product to add to cart: ");
-        String size = getInput("Type the size: ");
+        Integer userId = null;
+        Integer orderId = null;
+        Integer productId = null;
 
-        PreparedStatement prepStmt = conn.prepareStatement("SELECT namn, adress, nummer FROM Personuppgifter where namn = ?");
-        prepStmt.setString(1, "Kalle");
-        stmt.executeQuery();
+        PreparedStatement findUser = main.prepState("SELECT customer_id, firstname, lastname FROM Customers WHERE firstname = ? AND passwd = ?");
+        findUser.setString(1, userName);
+        findUser.setString(2, userPasswd);
+        ResultSet userResult = findUser.executeQuery();
 
-        ResultSet rs = stmt.executeQuery("SELECT customer_id FROM Customers WHERE firstName = ");
-        while (rs.next()) {
-            System.out.println(rs.getString("make")+ " " +rs.getString("model"));
-            System.out.println("Size: " + rs.getInt("size"));
-            System.out.println("In stock: " + rs.getInt("instock"));
-            System.out.println("Price: " + rs.getInt("price") + "\n");
+        while (userResult.next()) {
+            userId = userResult.getInt("customer_id");
+            System.out.println("Welcome " + userResult.getString("firstname") + " " + userResult.getString("lastname"));
+            System.out.println("Customer ID: " + userId);
         }
 
-        stmt.executeQuery("CALL AddToCart");
+        PreparedStatement findOrder = main.prepState("SELECT order_id FROM Orders WHERE customer_id = ?");
+        findOrder.setInt(1, userId);
+        ResultSet orderResult = findOrder.executeQuery();
+        while (orderResult.next()) {
+            orderId = orderResult.getInt("order_id");
+        }
+
+        main.viewProducts();
+        main.showCart(userId);
+        String make = getInput("Type the make of the product to add to cart: ");
+        String model = getInput("Type the model of the product to add to cart: ");
+        String size = getInput("Type the size of the product to add to cart: ");
+
+        PreparedStatement findProduct = main.prepState("SELECT product_id, make, model FROM Products WHERE make = ? AND model = ? AND size = ?");
+        findProduct.setString(1, make);
+        findProduct.setString(2, model);
+        findProduct.setString(3, size);
+        ResultSet productResult = findProduct.executeQuery();
+
+
+        while (productResult.next()) {
+            productId = productResult.getInt("product_id");
+            System.out.println("Adding " + productResult.getString("make") + " " + productResult.getString("model") + " to cart..." );
+        }
+        PreparedStatement addToCart = main.prepState("CALL AddToCart(?, ?, ?)");
+        if (userId == null)addToCart.setString(1, null);
+        else addToCart.setInt(1, userId);
+        if (orderId == null)addToCart.setString(2, null);
+        else addToCart.setInt(2, orderId);
+        if (productId == null)addToCart.setString(3, null);
+        else addToCart.setInt(3, productId);
+        ResultSet addToCartResult = addToCart.executeQuery();
+
     }
 
     public static String getInput(String message) throws Exception {
@@ -42,16 +79,40 @@ public class Main {
         return input;
     }
 
-    public void viewProducts(String message) throws Exception {
+    public void viewProducts() throws Exception {
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery("SELECT * FROM Products WHERE instock > 0");
         System.out.println("\n---Product List---\n");
         while (rs.next()) {
-            System.out.println(rs.getString("make")+ " " +rs.getString("model"));
+            System.out.println(rs.getString("make") + " " + rs.getString("model"));
             System.out.println("Size: " + rs.getInt("size"));
             System.out.println("In stock: " + rs.getInt("instock"));
             System.out.println("Price: " + rs.getInt("price") + "\n");
         }
+    }
+
+    public void showCart(Integer customerId) throws Exception {
+        System.out.println("\n---Your Cart---\n");
+        String sql = "SELECT p.make, p.model, p.size, p.price, pbo.quantity\n"+
+                "FROM Orders AS o \n" +
+                "JOIN ProductsPerOrders pbo ON pbo.order_id = o.order_id \n" +
+                "JOIN Products p ON p.product_id = pbo.product_id \n" +
+                "WHERE customer_id = ? \n" +
+                "GROUP BY p.model \n";
+
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, customerId);
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            System.out.println(rs.getString("make") + " " + rs.getString("model"));
+            System.out.println("Size: " + rs.getInt("size"));
+            System.out.println("Price: " + rs.getInt("price"));
+            System.out.println("Quantity: " + rs.getInt("quantity") + "\n");
+        }
+    }
+
+    public PreparedStatement prepState(String query) throws Exception {
+        return conn.prepareStatement(query);
     }
 
 }
